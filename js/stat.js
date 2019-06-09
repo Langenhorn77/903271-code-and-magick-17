cc6afd8b6c39ee5db5bf6b98a54ed42466aad572
Langenhorn77 <ch_ur@mail.ru>
2019-06-09 20:20:36 +0300

    Задание 2.

--- js/stat.js
+++ js/stat.js
@@ -16,12 +16,6 @@ var Bar = {
   gap: 50,
 };
 
-
-var renderCloud = function (ctx, x, y, color) {
-  ctx.fillStyle = color;
-  ctx.fillRect(x, y, Cloud.width, Cloud.height);
-};
-
 var renderText = function (ctx, x, y, font, size, text, base, style) {
   ctx.font = size + 'px ' + font || '16px PT Mono';
   ctx.textBaseline = base;
@@ -46,25 +40,29 @@ var getMaxElement = function (arr) {
   return maxElement;
 };
 
-window.renderStatistics = function (ctx, NAMES, TIMES) {
-  renderCloud(ctx, Cloud.x + GAP / 2, Cloud.y + GAP / 2, 'rgba(0, 0, 0, 0.7)');
-  renderCloud(ctx, Cloud.x, Cloud.y, '#fff');
+var xGap = Cloud.x + GAP;
+var yGap = Cloud.y + GAP;
+var xGapTwice = Cloud.x + GAP * 2;
 
-  renderText(ctx, Cloud.x + GAP, Cloud.y + GAP, ' PT Mono', 16, 'Ура вы победили!', 'hanging', '#000');
-  renderText(ctx, Cloud.x + GAP, Cloud.y + GAP + 16, ' PT Mono', 16, 'Список результатов:', 'hanging', '#000');
+var barProps = Bar.gap + Bar.width;
+
+var cloudBottom = Cloud.y + Cloud.height - TEXT_HEIGHT;
+
+window.renderStatistics = function (ctx, NAMES, TIMES) {
+  renderRect(ctx, Cloud.x + GAP / 2, Cloud.y + GAP / 2, Cloud.width, Cloud.height, 'rgba(0, 0, 0, 0.7)');
+  renderRect(ctx, Cloud.x, Cloud.y, Cloud.width, Cloud.height, '#fff');
+
+  renderText(ctx, xGap, yGap, ' PT Mono', 16, 'Ура вы победили!', 'hanging', '#000');
+  renderText(ctx, xGap, yGap + 16, ' PT Mono', 16, 'Список результатов:', 'hanging', '#000');
 
   var maxTime = getMaxElement(TIMES);
 
   for (var i = 0; i < NAMES.length; i++) {
-    renderText(ctx, Cloud.x + GAP * 2 + (Bar.gap + Bar.width) * i, Cloud.y + Cloud.height - TEXT_HEIGHT, ' PT Mono', 16, NAMES[i], 'hanging', '#000');
-    renderText(ctx, Cloud.x + GAP * 2 + (Bar.gap + Bar.width) * i, Cloud.y + Cloud.height - GAP * 2 - TEXT_HEIGHT - ((Bar.height * TIMES[i]) / maxTime), ' PT Mono', 16, Math.round(TIMES[i]), 'hanging', '#000');
+    renderText(ctx, xGapTwice + barProps * i, cloudBottom, ' PT Mono', 16, NAMES[i], 'hanging', '#000');
+    renderText(ctx, xGapTwice + barProps * i, cloudBottom - GAP * 2 - ((Bar.height * TIMES[i]) / maxTime), ' PT Mono', 16, Math.round(TIMES[i]), 'hanging', '#000');
 
-    if (NAMES[i] === 'Вы') {
-      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-    } else {
-      ctx.fillStyle = 'rgba(0, 0, 255, ' + Math.random().toFixed(2).toString() + ')';
-    }
-    renderRect(ctx, Cloud.x + GAP * 2 + (Bar.gap + Bar.width) * i, Cloud.y + Cloud.height - GAP - TEXT_HEIGHT, Bar.width, -((Bar.height * TIMES[i]) / maxTime));
+    renderRect(ctx, xGapTwice + barProps * i, cloudBottom - GAP, Bar.width, -((Bar.height * TIMES[i]) / maxTime), (NAMES[i] === 'Вы') ?
+      'rgba(255, 0, 0, 1)' : 'rgba(0, 0, 255, ' + Math.random().toFixed(2) + ')');
   }
 };
 
